6d2e52a9895a49da46d37dcdbfca5c77e3a5b478
Adam <ks20447@bristol.ac.uk>
2023-04-04 13:18:43 +0100

    Begun some profiling

--- week21.py
+++ week21.py
@@ -2,11 +2,17 @@ import numerical_differentiation as nd
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+import cProfile
 
 
 def week21_excercises():
     
-    def pde(x, t, u):
+    def pde_nonlinear(x, t, u):
+        f = 0
+        return f
+    
+    
+    def pde_linear(x, t):
         f = 0
         return f
     
@@ -18,25 +24,33 @@ def week21_excercises():
     d_coef = 0.1
     t_final = 2
     
-    start = time.perf_counter()
-    grid_1, time_1, u_exp_euler = nd.explicit_methods(pde, a, b, d_coef, bc_left, bc_right, ic, 100, t_final, "Euler")
-    elapsed = time.perf_counter() - start
-    print(f"{elapsed} seconds")
+    # start = time.perf_counter()
+    # grid_1, time_1, u_exp_euler = nd.explicit_methods(pde_nonlinear, a, b, d_coef, bc_left, bc_right, ic, 100, t_final, "Euler")
+    # elapsed = time.perf_counter() - start
+    # print(f"{elapsed} seconds")
     
-    start = time.perf_counter()
-    grid_2, time_2, u_runge_kuta = nd.explicit_methods(pde, a, b, d_coef, bc_left, bc_right, ic, 100, t_final, "RK4")
-    elapsed = time.perf_counter() - start
-    print(f"{elapsed} seconds")
+    # start = time.perf_counter()
+    # grid_2, time_2, u_runge_kuta = nd.explicit_methods(pde_nonlinear, a, b, d_coef, bc_left, bc_right, ic, 100, t_final, "RK4")
+    # elapsed = time.perf_counter() - start
+    # print(f"{elapsed} seconds")
         
-    start = time.perf_counter()
-    grid_3, time_3, u_imp_euler = nd.implicit_methods(pde, a, b, d_coef, bc_left, bc_right, ic, 100, 0.1, t_final, "Euler")
-    elapsed = time.perf_counter() - start
-    print(f"{elapsed} seconds")   
+    # start = time.perf_counter()
+    # grid_3, time_3, u_imp_euler = nd.implicit_methods(pde_linear, a, b, d_coef, bc_left, bc_right, ic, 100, 0.1, t_final, "Euler")
+    # elapsed = time.perf_counter() - start
+    # print(f"{elapsed} seconds")   
     
-    start = time.perf_counter()
-    grid_4, time_4, u_crank = nd.implicit_methods(pde, a, b, d_coef, bc_left, bc_right, ic, 100, 0.1, t_final, "Crank-Nicolson")
-    elapsed = time.perf_counter() - start
-    print(f"{elapsed} seconds")    
-
+    # start = time.perf_counter()
+    # grid_4, time_4, u_crank = nd.implicit_methods(pde_linear, a, b, d_coef, bc_left, bc_right, ic, 100, 0.1, t_final, "Crank-Nicolson")
+    # elapsed = time.perf_counter() - start
+    # print(f"{elapsed} seconds")    
+    
+    cp = cProfile.Profile()
+    cp.enable()
+    
+    grid_2, time_2, u_runge_kuta = nd.explicit_methods(pde_nonlinear, a, b, d_coef, bc_left, bc_right, ic, 100, t_final, "RK4")
+    
+    cp.disable()
+    cp.print_stats(sort="cumtime")
+    
     
 week21_excercises()
